37138c40a5b85ff2e4c782beeb80f65f4a1cfa3e
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-17 16:04:48 -0300

    Resolucao exercicio 04

---
+++ src/main/java/com/cubos/exercicios/exercicio_04/JurosCompostos.java
@@ -0,0 +1,25 @@
+package com.cubos.exercicios.exercicio_05;
+
+import java.util.Scanner;
+
+public class JurosCompostos {
+    public static void main(String[] args) {
+        double capital,taxaFixa,periodo;
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("Digite o capital:");
+            capital = s.nextDouble();
+
+            System.out.println("Digite a taxa fixa:");
+            taxaFixa = s.nextDouble();
+
+            System.out.println("Digite o periodo:");
+            periodo = s.nextDouble();
+
+            double montante = capital * Math.pow(1 + taxaFixa, periodo);
+
+            System.out.println(Math.round(montante));
+        }
+
+        
+    }
+}
