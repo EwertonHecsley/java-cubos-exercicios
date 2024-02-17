18c7da895a73d0a29a65f25f449a17450a482cb8
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-17 15:15:54 -0300

    Resolucao exercicio 2

---
+++ src/main/java/com/cubos/exercicios/exercicio_02/FahrenheitConvert.java
@@ -0,0 +1,18 @@
+package com.cubos.exercicios.exercicio_02;
+
+import java.util.Scanner;
+
+public class FahrenheitConvert {
+    public static void main(String[] args) {
+        double celsius;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em graus Fahrenheit:");
+        double fahrenheit = s.nextDouble();
+
+        celsius = Math.round((fahrenheit - 32) * 5/9);
+
+        System.out.println("A temperatura em Celsius é: " + celsius);
+    }
+}
