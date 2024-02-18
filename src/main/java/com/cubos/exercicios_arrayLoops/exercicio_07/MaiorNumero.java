fe221ee6f1f11dad2129b905fbbb11c7151fed6a
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-17 22:23:57 -0300

    resolucao

---
+++ src/main/java/com/cubos/exercicios_arrayLoops/exercicio_07/MaiorNumero.java
@@ -0,0 +1,19 @@
+package com.cubos.exercicios_arrayLoops.exercicio_07;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MaiorNumero {
+    public static void main(String[] args) {
+        Integer[] numeros = {3, 4, 1, 8, 11, 7, 5};
+        int maiorNumero;
+
+        Arrays.sort(numeros,Collections.reverseOrder());
+
+        System.out.println(numeros.getClass());
+
+        maiorNumero = numeros[0];
+
+        System.out.println(maiorNumero);
+    }
+}
